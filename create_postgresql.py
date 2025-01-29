51b7506b503d08429fefc300202a411394bec113
indiansinrichmondbc <162862425+indiansinrichmondbc@users.noreply.github.com>
2025-01-29 01:59:35 -0800

    Update create_postgresql.py

--- create_postgresql.py
+++ create_postgresql.py
@@ -5,11 +5,11 @@ def create_postgresql_db():
     try:
         # Connect to PostgreSQL
         conn = psycopg2.connect(
-            dbname=os.getenv("POSTGRES_DB"),
-            user=os.getenv("POSTGRES_USER"),
-            password=os.getenv("POSTGRES_PASSWORD"),
-            host=os.getenv("POSTGRES_HOST"),
-            port=os.getenv("POSTGRES_PORT")
+            dbname=os.getenv("AZURE_POSTGRES_DB"),
+            user=os.getenv("AZURE_POSTGRES_USER"),
+            password=os.getenv("AZURE_POSTGRES_PASSWORD"),
+            host=os.getenv("AZURE_POSTGRES_HOST"),
+            port=os.getenv("AZURE_POSTGRES_PORT")
         )
         cur = conn.cursor()
 
